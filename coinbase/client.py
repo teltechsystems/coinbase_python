f053a64d8b25ef46370626b1bd8a0cefda49a508
Bryan Moyles <bmoy117@gmail.com>
2014-02-14 23:22:07 -0500

    Removing data in Request so that the method transfers to GET, and setting scheme to https

--- coinbase/client.py
+++ coinbase/client.py
@@ -2,7 +2,7 @@ import json
 import urllib2
 
 def do_request(authenticator, method, path, data = {}):
-	request = urllib2.Request(url='http://coinbase.com/api/v1' + path, data=json.dumps(data))
+	request = urllib2.Request(url='https://coinbase.com/api/v1' + path, data=json.dumps(data) if data else None)
 	request.headers.update(authenticator.get_headers(request))
 
 	f = urllib2.urlopen(request)
